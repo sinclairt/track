9495b611dad14f0047b9fb1d13d66cc60765ad60
tom-sinclair <tom.sinclair31@gmail.com>
2016-03-22 11:08:18 +0000

    [Update] publish presenter

--- src/Sinclair/Track/TrackServiceProvider.php
+++ src/Sinclair/Track/TrackServiceProvider.php
@@ -3,48 +3,50 @@
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\AliasLoader;
 
-class TrackServiceProvider extends ServiceProvider {
+class TrackServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		AliasLoader::getInstance()->alias('Track', 'Sinclair\Track\Facades\Track');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        AliasLoader::getInstance()->alias('Track', 'Sinclair\Track\Facades\Track');
 
-		$this->publishes([
-			__DIR__.'/../../migrations/' => base_path('/database/migrations'),
-			__DIR__.'/../../config/' => config_path()
-		]);
-	}
+        $this->publishes([
+            __DIR__ . '/../../migrations/' => base_path('/database/migrations'),
+            __DIR__ . '/../../config/' => config_path(),
+            __DIR__ . '/TrackPresenter.php' => app_path('Presenters')
+        ]);
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->bind('Sinclair\Track\TrackInterface', 'Sinclair\Track\Track');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('Sinclair\Track\TrackInterface', 'Sinclair\Track\Track');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
